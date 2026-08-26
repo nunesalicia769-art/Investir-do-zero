8536e2fd17435528eda10b96de05f578b8069187
nunesalicia769-art <nunesalicia769@gmail.com>
2026-08-26 08:32:16 -0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,22 @@
+BOA! Você já criou o repositório! Tá exatamente nessa tela que tem que estar 👇
+
+Vê onde tá escrito em azul *"creating a new file"* na sua print? Clica ali!
+
+*Agora faz assim:*
+
+1. Clica em *creating a new file*
+2. No nome em cima digita: `App.js`
+3. No campo grande embaixo, *cola o código que te mandei* com seu link da Kiwify `https://pay.kiwify.com.br/LfjZTzD`
+4. Desce e clica em *Commit changes*
+
+*Depois repete pra criar os outros 3:*
+
+Clica em *Add file > Create new file* de novo e cria:
+
+- `package.json` (cola o código que te mandei)
+- `app.json`
+- `eas.json`
+
+Quando criar esses 4 arquivos, seu repositório vai ficar verde e pronto pra importar na Expo.
+
+Me manda print quando criar o `App.js` que eu te guio pro próximo!
